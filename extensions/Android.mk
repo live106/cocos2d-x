88a10e3ae3bc52cd1d7ba7de929126a419076aea
past106 <wangyanan@gpp.com>
2014-11-24 10:11:35 +0800

    增加第三方库freetype2

--- extensions/Android.mk
+++ extensions/Android.mk
@@ -155,10 +155,12 @@ spine/spine-cocos2dx.cpp \
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_curl_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_freetype2_static
 #LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
 #LOCAL_WHOLE_STATIC_LIBRARIES += chipmunk_static
 #LOCAL_WHOLE_STATIC_LIBRARIES += libwebsockets_static
 LOCAL_WHOLE_STATIC_LIBRARIES += wxsqlite3_static
+LOCAL_WHOLE_STATIC_LIBRARIES += android_support
 
 
 LOCAL_CFLAGS += -DCC_ENABLE_CHIPMUNK_INTEGRATION=1
@@ -181,6 +183,7 @@ include $(BUILD_STATIC_LIBRARY)
 $(call import-module,cocos2dx)
 $(call import-module,CocosDenshion/android)
 $(call import-module,cocos2dx/platform/third_party/android/prebuilt/libcurl)
+$(call import-module,cocos2dx/platform/third_party/android/prebuilt/libfreetype2)
 #$(call import-module,external/Box2D)
 #$(call import-module,external/chipmunk)
 #$(call import-module,external/libwebsockets/android)
