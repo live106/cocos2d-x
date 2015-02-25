946ab917223777cc191e9f17790df2831683aad2
past106 <wangyanan@gpp.com>
2015-02-25 09:56:15 +0800

    add lib icon support

---
+++ libiconv/Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := iconv_static
+LOCAL_MODULE_FILENAME := libiconv
+
+
+LOCAL_CFLAGS := \
+  -Wno-multichar \
+  -DAndroid \
+  -DLIBDIR="c" \
+  -DBUILDING_LIBICONV \
+  -DIN_LIBRARY
+
+
+LOCAL_SRC_FILES := \
+  libcharset/lib/localcharset.c \
+  lib/iconv.c \
+  lib/relocatable.c
+
+
+LOCAL_C_INCLUDES += \
+  $(LOCAL_PATH)/include \
+  $(LOCAL_PATH)/libcharset \
+  $(LOCAL_PATH)/lib \
+  $(LOCAL_PATH)/libcharset/include \
+  $(LOCAL_PATH)/srclib
+  
+include $(BUILD_STATIC_LIBRARY)
